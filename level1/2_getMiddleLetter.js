7a74dd69c40c519e162468cd8197b3b2cde8d1ac
Theo <theo@codymanager.com>
2020-03-16 17:05:16 +0900

    0316

---
+++ level1/2_getMiddleLetter.js
@@ -0,0 +1,22 @@
+/**
+문제 설명
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+재한사항
+s는 길이가 1 이상, 100이하인 스트링입니다.
+
+입출력 예
+s	return
+abcde	c
+qwer    we
+
+ */
+
+
+function solution(s) {
+    let half = parseInt(s.length / 2);
+    return s.length % 2 ? s.substring(half, half+1) : s.substring(half-1, half+1);
+}
+
+solution("abcde");
+solution("qwer");
